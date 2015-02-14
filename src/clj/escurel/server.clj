ee06d7299f909b1750dc945521d131bde5d14bdd
Ryan Kelker <theinternet@yandex.com>
2015-02-14 14:27:01 +0900

    refactored get-client

--- src/clj/escurel/server.clj
+++ src/clj/escurel/server.clj
@@ -58,15 +58,23 @@
 (defn existing-client [state-atom & ks]
   (get-in state-atom (reduce conj [:clients] ks)))
 
-(defn get-client [async-channel]
+(defn init-client-read-chan []
+  (chan (async/sliding-buffer 10)))
+
+(defn init-client-write-chan []
+  (chan (async/dropping-buffer 10)))
+
+(defn new-client []
+  {:ws-read (init-client-read-chan)
+   :ws-write  (init-client-write-chan)})
+
+(defn get-client! [async-channel]
   (let [ac (str async-channel)]
     (if-let [client (existing-client server-state ac)]
       (already-registered-client! ac client)
-      (let [new-client {:ws-read (chan (async/sliding-buffer 10))
-                        :ws-write (chan (async/dropping-buffer 10))}]
-        (adding-registered-client! server-state
-                                   ac
-                                   new-client)))))
+      (adding-registered-client! server-state
+                                 ac
+                                 (new-client)))))
 
 (defn connection-open-msg! [remote-addr ac]
   (println "Opened connection from"
@@ -80,7 +88,7 @@
                 websocket?]} req]
     (if-not websocket?
       "Sorry, websocket required"
-      (let [client (get-client async-channel)
+      (let [client (get-client! async-channel)
             {:keys [ws-read ws-write]} client]
         (connection-open-msg! remote-addr async-channel)
         (with-channel req ws-channel {:format :transit-json
